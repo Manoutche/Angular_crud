83983c02dd6178adf179b5d31678a26d18b591eb
Manoutche <67541888+Manoutche@users.noreply.github.com>
2024-12-03 16:55:19 +0000

    first part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, model, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Student } from './models/student.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  @ViewChild('modalId') modal : ElementRef | undefined /** element ref is add #modalId to localize the element  */
+  studentObj: Student = new Student()
+
+  closeModal() {
+    if (this.modal) {
+      this.modal.nativeElement.style.display = 'none'
+    }
+  }
+  openModal() {
+    const modal =  document.getElementById('modalId')
+    if (modal) {
+      modal.style.display = 'block'
+    }
+  }
+
+  saveStudent() {
+    const isLocalPresent = localStorage.getItem("angularCrud")
+    if (isLocalPresent) {
+      const oldStudent: Student [] = JSON.parse(isLocalPresent)
+      let name = this.studentObj.name
+      let email = this.studentObj.email
+      let findStudent = oldStudent.filter(student =>(student.name == name && student.email == email))
+      if (!findStudent) {
+        oldStudent.push(this.studentObj)
+        localStorage.setItem('angularCrud',JSON.stringify(oldStudent))
+      } else {
+        alert("Student already exist")
+      }
+    } else {
+      const newStudent = []
+      newStudent.push(this.studentObj)
+      localStorage.setItem('angularCrud',JSON.stringify(newStudent))
+    }
+  }
   title = 'Angular_crud';
 }
---
+++ src/app/models/student.model.ts
@@ -0,0 +1,19 @@
+export class Student {
+  name:string
+  mobileNo:string
+  email:string
+  city:string
+  pincode:string
+  state:string
+  address: string
+
+  constructor(){
+    this.name = ''
+    this.mobileNo = ''
+    this.email = ''
+    this.city = ''
+    this.pincode = ''
+    this.state = ''
+    this.address = ''
+  }
+}
